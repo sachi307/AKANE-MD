e926fe230c17c702a8a58e2c22c3b6ad10af7632
Richiethgoat <richiejnr57@gmail.com>
2025-06-19 13:54:16 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 
 //================= { SETTINGS } =================\\
 global.owner = process.env.OWNER_NUMBER || "234"; // owner number 
+global.botimg = 'https://files.catbox.moe/l9gpzm.jpg'
 global.sudo = process.env.SUDO  || " ";
 global.ownername = process.env.OWNER_NAME || "Richie";
 global.botname = process.env.BOT_NAME || "AKANE"
